889efb5ce944d656cac3c0cb7bb56d6d0dd15d25
Dopadofull <melquistar@gmail.com>
2023-10-12 10:45:52 -0300

    modidficacion postgres

---
+++ EjModificacion_postgres/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO "user"(username, password)
+	VALUES ('sas','ses'),
+			('sis', 'sos'),
+			('sus', 'asa'),
+			('ese', 'sis');
+
+--POST
+INSERT INTO "post"(author_id, title, body)
+	VALUES (1,'1111','1010'),
+			(2,'2222','2020'),
+		    (3,'3333','3030'),
+			(4,'4444','4040');
---
+++ EjModificacion_postgres/update.sql
@@ -0,0 +1,5 @@
+UPDATE "user" SET username = 'MELki'
+WHERE id = 1;
+--POST
+UPDATE "post" SET body = '2000'
+WHERE author_id >= 3;
